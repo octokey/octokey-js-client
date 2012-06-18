9f9a6a2e2539c6b05bec7e3d45dc890fd1af5293
Conrad Irwin <conrad.irwin@gmail.com>
2012-06-18 01:27:25 -0700

    Make it look Web 3.0

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -44,8 +44,27 @@
                         auth_request.val(response.auth_request);
                         form.submit();
                     } else if (response.handshake_id) {
-                        var div = $('<div/>');
-                        div.appendTo('body').qrcode("" + response.handshake_id);
+                        var div = $('<div/>').css({
+                            position: 'fixed',
+                            top: '50%',
+                            left: '50%',
+                            'margin-top': '-175px',
+                            'margin-left': '-175px',
+                            padding: '50px',
+                            'border-radius': '10px',
+                            'box-shadow': '0 0 20px rgba(0, 0, 0, 0.2)'
+                        }),
+                            mask = $('<div/>').css({
+                            'background-color': 'rgba(255,255,255, 0.6)',
+                            position: 'fixed',
+                            top: 0,
+                            left: 0,
+                            height: '100%',
+                            width: '100%'
+                        });
+                        mask.appendTo('body');
+                        div.appendTo(mask);
+                        div.qrcode("" + response.handshake_id);
                         sendRequest({
                             message: 'await_handshake',
                             handshake_id: response.handshake_id
